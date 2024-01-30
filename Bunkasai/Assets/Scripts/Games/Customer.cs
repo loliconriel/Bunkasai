aa158aec43ee09d60fc58c179c5b8fb76f3c7fdc
loliconriel <loliconriel@gmail.com>
2024-01-30 16:52:49 +0800

    Code_v7

--- Bunkasai/Assets/Scripts/Games/Customer.cs
+++ Bunkasai/Assets/Scripts/Games/Customer.cs
@@ -9,7 +9,7 @@ public class Customer : MonoBehaviour
     [SerializeField] Vector3 StayPosition;
     [SerializeField] Vector3 EndPosition;
 
-    public Customer(Vector3 Speed,Vector3 StayPosition,Vector3 EndPosition)
+    public void Init(Vector3 Speed,Vector3 StayPosition,Vector3 EndPosition)
     {
         this.Speed = Speed;
         this.StayPosition = StayPosition;
@@ -24,7 +24,11 @@ public class Customer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(transform.localPosition.x -StayPosition.x) < 1)
+        {
+
+        }
+        else transform.localPosition += Speed * Time.deltaTime;
     }
 
 
--- Bunkasai/Assets/Scripts/Games/CustomerGenerator.cs
+++ Bunkasai/Assets/Scripts/Games/CustomerGenerator.cs
@@ -26,14 +26,39 @@ public class CustomerGenerator : MonoBehaviour
         {
             if (Timer-(SpawnTime * Random.Range(0.9f, 1.1f)) > 0)
             {
-                
+                int counter = 0;
                 Timer = 0f;
-                int Spawnnumber = Random.Range(0, 1);
-                Transform SpawnPlace = SpawnList.transform.GetChild(Spawnnumber).transform;
-                GameObject a = Instantiate(Customer[Random.Range(0, Customer.Length)], SpawnPlace);
-                a.transform.SetParent(StayList.transform.GetChild(Random.Range(0, StayList.transform.childCount)));
-                Vector3 Speed = StayList.transform.GetChild(1).localPosition - StayList.transform.GetChild(0).localPosition;
-                //test = Spawnnumber == 0 ? new Customer(Speed, transform.parent.localPosition, SpawnList.transform.GetChild(1).localPosition) : new Customer(-Speed, transform.parent.localPosition, SpawnList.transform.GetChild(0).localPosition);
+                for (int i = 0; i < StayList.transform.childCount; i++)
+                {
+                    if (StayList.transform.GetChild(i).childCount != 0)
+                    {
+                        counter++;
+                    }
+                    else break;
+                }
+                if(counter != StayList.transform.childCount)
+                {
+                    int Spawnnumber = Random.Range(0, 2);
+                    int Spawnplace;
+                    do
+                    {
+                        Spawnplace = Random.Range(0, StayList.transform.childCount);
+                    } while (StayList.transform.GetChild(Spawnplace).childCount != 0);
+                    Transform SpawnPlace = SpawnList.transform.GetChild(Spawnnumber).transform;
+                    GameObject a = Instantiate(Customer[Random.Range(0, Customer.Length)], SpawnPlace);
+                    a.transform.SetParent(StayList.transform.GetChild(Spawnplace));
+                    Customer test = a.AddComponent<Customer>();
+                    Vector3 Speed = StayList.transform.GetChild(1).localPosition - StayList.transform.GetChild(0).localPosition;
+                    if (Spawnnumber == 0)
+                    {
+                        test.Init(Speed, transform.parent.localPosition, SpawnList.transform.GetChild(1).localPosition);
+                    }
+                    else test.Init(-Speed, transform.parent.localPosition, SpawnList.transform.GetChild(0).localPosition);
+                }
+                else
+                {
+                    
+                }
                 
             }
         }
