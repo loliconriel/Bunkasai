d1ee249bb71c527d21715601589794bdeee49d21
loliconriel <loliconriel@gmail.com>
2024-01-30 15:31:03 +0800

    code_v6

--- Bunkasai/Assets/Scripts/Games/Customer.cs
+++ Bunkasai/Assets/Scripts/Games/Customer.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Customer : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 Speed;
+    [SerializeField] Vector3 StayPosition;
+    [SerializeField] Vector3 EndPosition;
 
-    Vector3 StartPosition;
-    Vector3 StayPosition;
-    Vector3 EndPosition;
-
-    public Customer(Vector3 StayPosition,Vector3 EndPosition)
+    public Customer(Vector3 Speed,Vector3 StayPosition,Vector3 EndPosition)
     {
+        this.Speed = Speed;
         this.StayPosition = StayPosition;
         this.EndPosition = EndPosition;
+        Debug.Log(this.Speed + "  " + this.StayPosition+"  "+this.EndPosition);
     }
     void Start()
     {
@@ -23,10 +25,6 @@ public class Customer : MonoBehaviour
     void Update()
     {
         
-    }
-    public void CustomerInit()
-    {
-
     }
 
 
--- Bunkasai/Assets/Scripts/Games/CustomerGenerator.cs
+++ Bunkasai/Assets/Scripts/Games/CustomerGenerator.cs
@@ -28,8 +28,13 @@ public class CustomerGenerator : MonoBehaviour
             {
                 
                 Timer = 0f;
-                Transform SpawnPlace = SpawnList.transform.GetChild(Random.Range(0, 2)).transform;
-                var a = Instantiate(Customer[Random.Range(0, Customer.Length)], SpawnPlace);
+                int Spawnnumber = Random.Range(0, 1);
+                Transform SpawnPlace = SpawnList.transform.GetChild(Spawnnumber).transform;
+                GameObject a = Instantiate(Customer[Random.Range(0, Customer.Length)], SpawnPlace);
+                a.transform.SetParent(StayList.transform.GetChild(Random.Range(0, StayList.transform.childCount)));
+                Vector3 Speed = StayList.transform.GetChild(1).localPosition - StayList.transform.GetChild(0).localPosition;
+                //test = Spawnnumber == 0 ? new Customer(Speed, transform.parent.localPosition, SpawnList.transform.GetChild(1).localPosition) : new Customer(-Speed, transform.parent.localPosition, SpawnList.transform.GetChild(0).localPosition);
+                
             }
         }
         
